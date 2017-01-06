6bd4d7df40694ed43fd8af3930b06d5a8f380163
Shubham <shubham2892@gmail.com>
2017-01-06 13:11:57 -0500

    code indentation

--- background.js
+++ background.js
@@ -1,184 +1,191 @@
-
 var myTabs = (function () {
 
-  // Instance stores a reference to the Singleton
-  var instance;
+    // Instance stores a reference to the Singleton
+    var instance;
 
-  function init() {
-      var tabList = [];
-      return {
-        getGlobalTabList: function() {
-          return tabList;
-        }
-      };
-  }
-
-  return {
-
-    // Get the Singleton instance if one exists
-    // or create one if it doesn't
-    getInstance: function () {
-
-      if ( !instance ) {
-        instance = init();
-      }
-
-      return instance;
+    function init() {
+        var tabList = [];
+        return {
+            getGlobalTabList: function () {
+                return tabList;
+            }
+        };
     }
-  };
+
+    return {
+
+        // Get the Singleton instance if one exists
+        // or create one if it doesn't
+        getInstance: function () {
+
+            if (!instance) {
+                instance = init();
+            }
+
+            return instance;
+        }
+    };
 })();
 
 
-function getTabList(){
-  return myTabs.getInstance().getGlobalTabList();
+function getTabList() {
+    return myTabs.getInstance().getGlobalTabList();
 }
 
 
-function switchToNextTab(){
-  var tabList = getTabList();
-  switchTabs(tabList[1].id);
-}
-
-function indexOfTab(tabId){
-  var tabList = getTabList();
-  for (var j =0; j< tabList.length; j++){
-    if (tabList[j].id === tabId){
-      return j;
+function switchToNextTab() {
+    var tabList = getTabList();
+    if (tabList.length > 1) {
+        switchTabs(tabList[1].id);
     }
-  }
-  return -1;
 }
 
-function switchTabs(tabId){
-  var index = indexOfTab(parseInt(tabId,10));
-  var windowId = getTabList()[index].windowId;
-  chrome.windows.update(windowId, {focused:true}, function () {
-    chrome.tabs.update(parseInt(tabId,10), {active:true});
-  });
-}
-
-
-function recordTabsRemoved(tabId){
-  var tabList = getTabList();
-  var idx = indexOfTab(tabId);
-  if (idx >= 0){
-    tabList.splice(idx,1);
-  }
-}
-
-function recordTabsAdded(tab){
-  var tabList = getTabList();
-  tabList.push(tab);
-}
-
-function recordTabsUpdated(tabId){
-  var idx = indexOfTab(tabId);
-  var tabList = getTabList();
-  console.log(idx);
-  if (idx >= 0){
-    var tabs = tabList.splice(idx,1);
-    console.log(tabs);
-    tabList.unshift(tabs[0]);
-  }
-}
-
-function init(){
-
-  // count and record all the open tabs for all the windows
-  chrome.windows.getAll({populate:true}, function (windows) {
-
-    for(var i = 0; i < windows.length; i++) {
-      var t = windows[i].tabs;
-      console.log("new window");
-      for(var j = 0; j < t.length; j++) {
-        console.log("new tab");
-        recordTabsAdded(t[j]);
-      }
-
+function indexOfTab(tabId) {
+    var tabList = getTabList();
+    for (var j = 0; j < tabList.length; j++) {
+        if (tabList[j].id === tabId) {
+            return j;
+        }
     }
+    return -1;
+}
 
-    // set the current tab as the first item in the tab list
-    chrome.tabs.query({currentWindow:true, active:true}, function(tabArray) {
-      // log('initial selected tab', tabArray);
-      recordTabsUpdated(tabArray[0].id);
+function switchTabs(tabId) {
+    var index = indexOfTab(parseInt(tabId, 10));
+    var windowId = getTabList()[index].windowId;
+    chrome.windows.update(windowId, {focused: true}, function () {
+        chrome.tabs.update(parseInt(tabId, 10), {active: true});
     });
-  });
+}
 
 
-  // attach an event handler to capture tabs as they are closed
-  chrome.tabs.onRemoved.addListener(function(tabId) {
-    console.log("------------------------------------------------------");
-    console.log("TabId Removed:");
-    console.log(tabId);
-    recordTabsRemoved(tabId);
-    console.log(getTabList());
-    console.log("------------------------------------------------------");
-
-  });
-
-  // attach an event handler to capture tabs as they are opened
-  chrome.tabs.onCreated.addListener(function (tab) {
-    if (tab.id !== chrome.tabs.TAB_ID_NONE ){
-      console.log("------------------------------------------------------");
-      console.log("TabId created:");
-      console.log(tab.id);
-      recordTabsAdded(tab);
-      console.log(getTabList());
-      console.log("------------------------------------------------------");
+function recordTabsRemoved(tabId) {
+    var tabList = getTabList();
+    var idx = indexOfTab(tabId);
+    if (idx >= 0) {
+        tabList.splice(idx, 1);
     }
+}
 
-  });
+function recordTabsAdded(tab) {
+    var tabList = getTabList();
+    tabList.push(tab);
+}
 
-  chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    if(changeInfo.status == 'complete' && tab.status == 'complete' && tab.url != undefined){
-      console.log("------------------------------------------------------");
-      console.log("TabId Updated:");
-      console.log(tabId);
-      var tabList = getTabList();
-      var idx = indexOfTab(tabId);
-      if (idx >= 0){
-        tabList[idx] = tab;
-      }
-      console.log(getTabList());
-      console.log("------------------------------------------------------");
+function recordTabsActivated(tabId) {
+    var idx = indexOfTab(tabId);
+    var tabList = getTabList();
+    console.log(idx);
+    if (idx >= 0) {
+        var tabs = tabList.splice(idx, 1);
+        console.log(tabs);
+        tabList.unshift(tabs[0]);
     }
-  });
+}
 
-  chrome.tabs.onActivated.addListener(function (info) {
-    console.log("------------------------------------------------------");
-    console.log("TabId Activated:");
-    console.log(info.tabId);
-    recordTabsUpdated(info.tabId);
-    console.log(getTabList());
-    console.log("------------------------------------------------------");
+function init() {
 
-  });
+    // count and record all the open tabs for all the windows
+    chrome.windows.getAll({populate: true}, function (windows) {
 
-  chrome.windows.onFocusChanged.addListener(function(windowId) {
-    if (windowId != chrome.windows.WINDOW_ID_NONE) {
-      chrome.tabs.query({windowId:windowId, active:true}, function (tabArray) {
-        if (tabArray.length === 1){
-          console.log("------------------------------------------------------");
-          console.log("TabId Activated(Window Changed):");
-          console.log(tabArray[0].id);
-          recordTabsUpdated(tabArray[0].id);
-          console.log(getTabList());
-          console.log("------------------------------------------------------");
+        for (var i = 0; i < windows.length; i++) {
+            var t = windows[i].tabs;
+            console.log("new window");
+            for (var j = 0; j < t.length; j++) {
+                console.log("new tab");
+                recordTabsAdded(t[j]);
+            }
+
         }
 
-      });
-    }
-  });
-
-  chrome.commands.onCommand.addListener(function(command) {
-    //log('Command:', command);
-
-    if (command === "quick-next-tab") {
-      switchToNextTab();
-    }
+        // set the current tab as the first item in the tab list
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabArray) {
+            // log('initial selected tab', tabArray);
+            recordTabsActivated(tabArray[0].id);
+        });
+    });
 
 
-  });
+    // attach an event handler to capture tabs as they are closed
+    chrome.tabs.onRemoved.addListener(function (tabId) {
+        console.log("------------------------------------------------------");
+        console.log("TabId Removed:");
+        console.log(tabId);
+        recordTabsRemoved(tabId);
+        console.log(getTabList());
+        console.log("------------------------------------------------------");
+
+    });
+
+    // attach an event handler to capture tabs as they are opened
+    chrome.tabs.onCreated.addListener(function (tab) {
+
+        if (tab.id !== chrome.tabs.TAB_ID_NONE) {
+            console.log("------------------------------------------------------");
+            console.log("TabId created:");
+            console.log(tab.id);
+            recordTabsAdded(tab);
+            console.log(getTabList());
+            console.log("------------------------------------------------------");
+        }else{
+            console.log("Null tab id created");
+            console.log(tab.id);
+        }
+
+    });
+
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete' && tab.status == 'complete') {
+            console.log("------------------------------------------------------");
+            console.log("TabId Updated:");
+            console.log(tabId);
+            var tabList = getTabList();
+            var idx = indexOfTab(tabId);
+            if (idx >= 0) {
+                tabList[idx] = tab;
+            }else{
+                console.log("Tab not found");
+            }
+            console.log(getTabList());
+            console.log("------------------------------------------------------");
+        }
+    });
+
+    chrome.tabs.onActivated.addListener(function (info) {
+        console.log("------------------------------------------------------");
+        console.log("TabId Activated:");
+        console.log(info.tabId);
+        recordTabsActivated(info.tabId);
+        console.log(getTabList());
+        console.log("------------------------------------------------------");
+
+    });
+
+    chrome.windows.onFocusChanged.addListener(function (windowId) {
+        if (windowId != chrome.windows.WINDOW_ID_NONE) {
+            chrome.tabs.query({windowId: windowId, active: true}, function (tabArray) {
+                if (tabArray.length === 1) {
+                    console.log("------------------------------------------------------");
+                    console.log("TabId Activated(Window Changed):");
+                    console.log(tabArray[0].id);
+                    recordTabsActivated(tabArray[0].id);
+                    console.log(getTabList());
+                    console.log("------------------------------------------------------");
+                }
+
+            });
+        }
+    });
+
+    chrome.commands.onCommand.addListener(function (command) {
+        //log('Command:', command);
+
+        if (command === "quick-next-tab") {
+            switchToNextTab();
+        }
+
+
+    });
 
 }
 
